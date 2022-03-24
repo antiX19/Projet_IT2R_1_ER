37380c394010d219ebb71e7c120ded0afdd5c272
Guillaume de Maricourt <51048232+Widelx@users.noreply.github.com>
2022-03-24 15:07:35 +0100

    Ajoute les projets vierges
    Ajoute des projets vierges afin de travailler sur les différentes parties de programme.

---
+++ Projet_keil_LPC1768/RTE/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Projet_keil_LPC1768' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define RTE_DEVICE_STARTUP_LPC17XX      /* Device Startup for NXP17XX */
+
+#endif /* RTE_COMPONENTS_H */
