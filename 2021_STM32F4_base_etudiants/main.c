f5fa80f68f68b0a2c6a343f2026728364f1bab62
Antoine E <78554532+antiX19@users.noreply.github.com>
2022-04-05 17:09:41 +0200

    Update main.c

--- 2021_STM32F4_base_etudiants/main.c
+++ 2021_STM32F4_base_etudiants/main.c
@@ -1,43 +1,3 @@
-/**
-  ******************************************************************************
-  * @file    Templates/Src/main.c 
-  * @author  MCD Application Team
-  * @brief   STM32F4xx HAL API Template project 
-  *
-  * @note    modified by ARM
-  *          The modifications allow to use this file as User Code Template
-  *          within the Device Family Pack.
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; COPYRIGHT(c) 2017 STMicroelectronics</center></h2>
-  *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *   1. Redistributions of source code must retain the above copyright notice,
-  *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
-  *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its contributors
-  *      may be used to endorse or promote products derived from this software
-  *      without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  *
-  ******************************************************************************
-  */
-
-/* Includes ------------------------------------------------------------------*/
 #include "Board_LED.h"                  // ::Board Support:LED
 #include "Driver_SPI.h"
 #include "stm32f4xx_hal.h"
@@ -47,12 +7,13 @@ extern ARM_DRIVER_SPI Driver_SPI1;
 void Configure_GPIO(void);
 void configure_ADC2_Channel_1(void);
 void Delay_ms(volatile int time_ms);
-void clignogauche(void);
-void clignodroit(void);
-void phare (void);
+void clignogauche(char LED_G [], int x);
+void clignodroit(char LED_D [], int y);
+void phare (char phares []);
+void feux_de_recul (char recul []);
+
 ADC_HandleTypeDef myADC2Handle;
 
-char tab[22+24];
 
 void Init_SPI(void)
 	  {
@@ -73,12 +34,11 @@ void Init_SPI(void)
   * @retval None
   */
 
-	  
 int main(void)
 {
-  
-  int i, nb_led;
-  
+  char tab[50];
+  short pumpmyride;
+  int i;
   Init_SPI();
   
 	Configure_GPIO(); // initialize PA0 pin 
@@ -86,30 +46,24 @@ int main(void)
 	LED_Initialize();
 
   while(1){
-		
-		for (i=0;i<4;i++)
+	  for (i=0;i<4;i++)
 			{
 			tab[i] = 0;
 			}
-	
-		phare();
-		clignogauche ();	
-		clignodroit ();
+		 phare(tab);
+	     feux_de_recul (tab);
+		clignogauche(tab, 3);
+		//clignodroit(tab, 3);
+	    tab[48] = 0;
+		tab[49] = 0;
 		
-		// end
-		tab[44] = 0;
-		tab[45] = 0;
-		
-		//envoie de la trame
-		Driver_SPI1.Send(tab,46);
-	    
   }
 }
 
-void phare (void)
+void phare (char phares [])
 		{
 		uint32_t Adc_value;
-		float voltage;
+		double voltage;
 		int nb_led;
 			
 		HAL_ADC_Start(&myADC2Handle); // start A/D conversion
@@ -121,66 +75,130 @@ void phare (void)
 			}
 			
 		HAL_ADC_Stop(&myADC2Handle); // stop conversion 
-		Delay_ms(200);
-		
+		Delay_ms(120);
+			
 		
 		for (nb_led = 0; nb_led <4;nb_led++)
-			{
-				tab[4+nb_led*4]=0xff;
-				tab[5+nb_led*4]=0x00;
-				tab[6+nb_led*4]=0x0E;
-				tab[7+nb_led*4]=0x0E;
-			}
-
-		if( voltage < 2.5)
+				{
+					phares[4+nb_led*4]=0xff;	//Allumage "normal" des phares 
+					phares[5+nb_led*4]=0x00;
+					phares[6+nb_led*4]=0x0e;
+					phares[7+nb_led*4]=0x0e;
+				}
+		if( voltage <= 2.5)
 		{
 		for (nb_led = 0; nb_led <4;nb_led++)
 				{
-					tab[4+nb_led*4]=0xff;
-					tab[5+nb_led*4]=0x00;
-					tab[6+nb_led*4]=0xff;
-					tab[7+nb_led*4]=0xff;
+					phares[4+nb_led*4]=0xff;	//Allumage plein phares
+					phares[5+nb_led*4]=0x00;
+					phares[6+nb_led*4]=0xff;
+					phares[7+nb_led*4]=0xff;
 				}
-			}	
 		}
-void clignogauche(void)
-		{
-		int nb_led;
-        
+		Driver_SPI1.Send(phares,50);	
+}
+		
+void feux_de_recul (char recul [])
+{
+	    uint32_t Adc_value;
+		double voltage;
 			
-		for (nb_led = 0; nb_led <3;nb_led++){
-			tab[20+nb_led*4]=0xff;
-			tab[21+nb_led*4]=0x00;
-			tab[22+nb_led*4]=0x00;
-			tab[23+nb_led*4]=0x00;
+		HAL_ADC_Start(&myADC2Handle); // start A/D conversion
+	  
+	  	if(HAL_ADC_PollForConversion(&myADC2Handle, 5) == HAL_OK) //check if conversion is completed
+			{
+			Adc_value  = HAL_ADC_GetValue(&myADC2Handle); // read digital value and save it inside uint32_t variable
+			voltage = (Adc_value * 8 ) / 10000;
 			}
+			
+		HAL_ADC_Stop(&myADC2Handle); // stop conversion 
+		Delay_ms(120);
 		
-		for (nb_led = 0; nb_led <3;nb_led++){
-			tab[32+nb_led*4]=0xff;
-			tab[33+nb_led*4]=0x00;
-			tab[34+nb_led*4]=0x3f;
-			tab[35+nb_led*4]=0xff;
-			}
-		}
-
-	
-void clignodroit(void)
+					recul[44]=0xff;	//Eteint feu de recul
+					recul[45]=0x00;
+					recul[46]=0x00;
+					recul[47]=0x00;
+		if( voltage <= 2.5)
 		{
-		int nb_led;
-        
-		for (nb_led = 0; nb_led <3;nb_led++){
-			tab[20+nb_led*4]=0xff;
-			tab[21+nb_led*4]=0x00;
-			tab[22+nb_led*4]=0x3f;
-			tab[23+nb_led*4]=0xff;
+		
+					recul[44]=0xff;	//Allumage feu de recul
+					recul[45]=0x00;
+					recul[46]=0x00;
+					recul[47]=0xff;
+				
+		}
+		Driver_SPI1.Send(recul,50);	
+}
+		
+
+void clignogauche(char LED_G[],int x)
+		{
+		int nb_led, i,j;
+						
+	for (nb_led = 0; nb_led <x;nb_led++){
+			LED_G[20+nb_led*4]=0xff;	
+			LED_G[21+nb_led*4]=0x00;
+			LED_G[22+nb_led*4]=0x00;
+			LED_G[23+nb_led*4]=0x00;
 			}
 		
-		for (nb_led = 0; nb_led <3;nb_led++){
-			tab[32+nb_led*4]=0xff;
-			tab[33+nb_led*4]=0x00;
-			tab[34+nb_led*4]=0x00;
-			tab[35+nb_led*4]=0x00;
+	for (nb_led = 0; nb_led <x;nb_led++){
+			LED_G[32+nb_led*4]=0xff;	
+			LED_G[33+nb_led*4]=0x00;
+			LED_G[34+nb_led*4]=0x3f;
+			LED_G[35+nb_led*4]=0xff;
+		}
+		Driver_SPI1.Send(LED_G,78);
+		 for(i = 0; i < 400000; i++)
+		{}
+								
+
+	for (nb_led = 0; nb_led <x;nb_led++){
+			LED_G[32+nb_led*4]=0xff;
+			LED_G[33+nb_led*4]=0x00;
+			LED_G[34+nb_led*4]=0x00;
+			LED_G[35+nb_led*4]=0x00;
+		}
+		Driver_SPI1.Send(LED_G,50);
+		 for(j = 0; j < 300000; j++)
+		{}
+			
+		
+		
+}
+  
+void clignodroit(char LED_D[],int y)
+		{
+		int nb_led, i,j;
+						
+	for (nb_led = 0; nb_led <y;nb_led++){
+			LED_D[20+nb_led*4]=0xff;
+			LED_D[21+nb_led*4]=0x00;
+			LED_D[22+nb_led*4]=0x3f;
+			LED_D[23+nb_led*4]=0xff;
 			}
+		
+	for (nb_led = 0; nb_led <y;nb_led++){
+			LED_D[32+nb_led*4]=0xe0;
+			LED_D[33+nb_led*4]=0x00;
+			LED_D[34+nb_led*4]=0x00;
+			LED_D[35+nb_led*4]=0x00;
+		}
+		Driver_SPI1.Send(LED_D,78);
+		 for(i = 0; i < 400000; i++)
+		{}
+								
+	for (nb_led = 0; nb_led <y;nb_led++){
+			LED_D[20+nb_led*4]=0xe0;
+			LED_D[21+nb_led*4]=0x00;
+			LED_D[22+nb_led*4]=0x00;
+			LED_D[23+nb_led*4]=0x00;
+			}
+		
+	
+		Driver_SPI1.Send(LED_D,50);
+		 for(j = 0; j < 300000; j++)
+		{}
 		}
 /* Configuration and initialization PA1 pin as analog input pin for A/D conversion */
 void Configure_GPIO(void)
